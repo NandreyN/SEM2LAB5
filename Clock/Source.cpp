ba5997f6e24990e1a63881a830a6d5a12b6036d6
NandreyN <nandreyn1998@gmail.com>
2017-03-07 21:37:54 +0300

    Task2 implemented

---
+++ Clock/Source.cpp
@@ -0,0 +1,190 @@
+#include <windows.h>
+#include <string>
+
+using namespace std;
+#define TIMER1 1
+#define MYKEY 1
+
+BOOL InitApplication(HINSTANCE hinstance);
+BOOL InitInstance(HINSTANCE hinstance, int nCMdShow);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT messagem, WPARAM wparam, LPARAM lparam);
+void DrawCircle(HDC& hdc, RECT& rect);
+void DisplayTime(HDC& hdc, RECT& rect, int seconds);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg;
+	InitApplication(hInstance);
+	InitInstance(hInstance, nCmdShow);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+	static bool ifTicks;
+	static int x, y;
+	static HDC hdc;
+	PAINTSTRUCT ps;
+	static RECT clientRect;
+	static int seconds;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		ifTicks = true;
+		SetTimer(hwnd, TIMER1, 1000, (TIMERPROC)NULL);
+		break;
+	case  WM_SIZE:
+		x = LOWORD(lparam);
+		y = HIWORD(lparam);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		GetClientRect(hwnd, &clientRect);
+		DrawCircle(hdc, clientRect);
+		DisplayTime(hdc, clientRect, seconds);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_HOTKEY:
+		switch(wparam)
+		{
+		case MYKEY:
+			seconds = -1;
+			break;
+		}
+		break;
+	case WM_CHAR:
+		switch (wparam)
+		{
+		case '-':
+			ifTicks = false;
+			break;
+		case '+':
+			ifTicks = true;
+			break;
+		}
+
+		break;
+	case WM_TIMER:
+		switch (wparam)
+		{
+		case TIMER1:
+		{
+			if (!ifTicks) break;
+			seconds++;
+			InvalidateRect(hwnd, NULL, true);
+			break;
+		}
+		default: break;
+		}
+		break;
+
+	case  WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		KillTimer(hwnd, TIMER1);
+		UnregisterHotKey(hwnd, MYKEY);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wparam, lparam);
+	}
+	return FALSE;
+}
+
+BOOL InitApplication(HINSTANCE hinstance)
+{
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hinstance;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = (LPSTR)"Clock";
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszMenuName = NULL;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Unable to init app", "Error", MB_OK);
+		return FALSE;
+	}
+	return TRUE;
+}
+BOOL InitInstance(HINSTANCE hinstance, int nCMdShow)
+{
+	HWND hwnd; hwnd = CreateWindow(
+		"Clock",
+		"Clock",
+		WS_OVERLAPPEDWINDOW,
+		0,
+		0,
+		600,
+		600,
+		NULL,
+		NULL,
+		hinstance,
+		NULL
+	);
+
+	if (!hwnd)
+	{
+		MessageBox(NULL, "Cannot init instance", "Error", MB_OK);
+		return FALSE;
+	}
+
+	ShowWindow(hwnd, nCMdShow);
+	UpdateWindow(hwnd);
+	RegisterHotKey(hwnd, MYKEY, MOD_SHIFT, 'S');
+	return TRUE;
+}
+void DrawCircle(HDC& hdc, RECT& rect)
+{
+	HPEN newPen, oldPen;
+	newPen = CreatePen(PS_SOLID, 3, BLACK_PEN);
+	oldPen = (HPEN)SelectObject(hdc, newPen);
+	Ellipse(hdc, rect.left, rect.top, rect.right, rect.bottom);
+	SelectObject(hdc, oldPen);
+	DeleteObject(newPen);
+}
+
+void DisplayTime(HDC& hdc, RECT& rect, int seconds)
+{
+	int minutes = seconds / 60;
+	seconds %= 60;
+	string displayText;
+	displayText += to_string(minutes) + ":" + to_string(seconds);
+
+	LOGFONT lf;
+	lf.lfWidth = (0.8*rect.right) / displayText.size();
+	lf.lfHeight = lf.lfWidth * 1.5;
+	lf.lfCharSet = DEFAULT_CHARSET;
+	lf.lfClipPrecision = CLIP_DEFAULT_PRECIS;
+	lf.lfEscapement = 0;
+	lf.lfItalic = 0;
+	lf.lfOrientation = 0;
+	lf.lfOutPrecision = OUT_DEFAULT_PRECIS;
+	lf.lfPitchAndFamily = DEFAULT_PITCH;
+	lf.lfQuality = DEFAULT_QUALITY;
+	lf.lfStrikeOut = 0;
+	lf.lfUnderline = 0;
+
+	HFONT newFont = CreateFontIndirect(&lf);
+	HFONT oldFont = (HFONT)SelectObject(hdc, newFont);
+
+	DrawText(hdc, displayText.data(), displayText.size(), &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+	SelectObject(hdc, oldFont);
+	DeleteObject(newFont);
+}
