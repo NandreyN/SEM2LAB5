200a056943f954ab7cd6f9e16e7ec951c041f72a
NandreyN <nandreyn1998@gmail.com>
2017-03-10 00:21:11 +0300

    Drawing circles

--- Circles/Source.cpp
+++ Circles/Source.cpp
@@ -1,11 +1,23 @@
 #include <windows.h>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+struct Circle
+{
+	Circle() { R = -1; };
+	POINT center;
+	int R;
+};
 
 BOOL InitApplication(HINSTANCE hinstance);
 BOOL InitInstance(HINSTANCE hinstance, int nCMdShow);
 LRESULT CALLBACK WndProc(HWND hwnd, UINT messagem, WPARAM wparam, LPARAM lparam);
+Circle DrawCircle(HDC& hdc, int x, int y, int R);
+bool IsInCircle(int x, int y, Circle& circle);
+Circle GetCircle(vector<Circle>& circles, int x, int y);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -23,13 +35,40 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine,
 }
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
+	static int x, y;
+	static HDC hdc;
+	static vector<Circle> circleCollection;
+	Circle c;
+
 	switch (message)
 	{
+	case WM_CREATE:
+		hdc = GetDC(hwnd);
+		break;
+	case WM_LBUTTONUP:
+		int xC; xC = LOWORD(lparam);
+		int yC;  yC = HIWORD(lparam);
+
+		 c = GetCircle(circleCollection, xC, yC);
+		if (c.R == -1)
+		{
+			circleCollection.push_back(DrawCircle(hdc, xC, yC, 5));
+		}
+		else
+		{
+			circleCollection.push_back(DrawCircle(hdc, c.center.x, c.center.y, c.R + 5));
+		}
+
+		break;
+	case WM_SIZE:
+		x = LOWORD(lparam);
+		y = HIWORD(lparam);
+		break;
 	case  WM_CLOSE:
 		DestroyWindow(hwnd);
+		ReleaseDC(hwnd, hdc);
 		break;
 	case WM_DESTROY:
-
 		PostQuitMessage(0);
 		break;
 	default:
@@ -86,4 +125,32 @@ BOOL InitInstance(HINSTANCE hinstance, int nCMdShow)
 	ShowWindow(hwnd, nCMdShow);
 	UpdateWindow(hwnd);
 	return TRUE;
+}
+Circle DrawCircle(HDC& hdc, int x0, int y0, int R)
+{
+	Circle c; POINT p; p.x = x0; p.y = y0;
+	c.center = p; c.R = R;
+	Ellipse(hdc, p.x - R, p.y - R, p.x + R, p.y + R);
+	return c;
+}
+
+bool IsInCircle(int x, int y, Circle& circle)
+{
+	int dist = sqrt(pow((circle.center.x - x), 2) + pow((circle.center.y - y), 2));
+	return (dist <= circle.R) ? true : false;
+}
+
+Circle GetCircle(vector<Circle>& circles, int x, int y)
+{
+	vector<Circle>::iterator iter = find_if(circles.begin(), circles.end(), [x, y](Circle c)
+	{
+		return IsInCircle(x, y, c);
+	});
+	Circle c;
+	if (iter != circles.end())
+	{
+		c = *iter;
+		circles.erase(iter);
+	}
+	return c;
 }
