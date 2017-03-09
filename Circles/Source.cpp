2adaa4a1c09513234bab0d00440137bcb69b3cf2
NandreyN <nandreyn1998@gmail.com>
2017-03-10 00:42:26 +0300

    Task4

--- Circles/Source.cpp
+++ Circles/Source.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <sstream>
 
 using namespace std;
 
@@ -11,13 +15,28 @@ struct Circle
 	POINT center;
 	int R;
 };
+ostream& operator<<(ostream& o, Circle c)
+{
+	o << c.center.x << " " << c.center.y << " " << c.R;
+	return o;
+}
 
+istream& operator >> (istream& i, Circle& c)
+{
+	string data;
+	getline(i, data);
+	istringstream iss(data);
+	iss >> c.center.x >> c.center.y >> c.R;
+	return i;
+}
 BOOL InitApplication(HINSTANCE hinstance);
 BOOL InitInstance(HINSTANCE hinstance, int nCMdShow);
 LRESULT CALLBACK WndProc(HWND hwnd, UINT messagem, WPARAM wparam, LPARAM lparam);
 Circle DrawCircle(HDC& hdc, int x, int y, int R);
 bool IsInCircle(int x, int y, Circle& circle);
 Circle GetCircle(vector<Circle>& circles, int x, int y);
+void SaveToFile(vector<Circle>& circles);
+void LoadFromFile(vector<Circle>& circles);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -44,12 +63,16 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 	{
 	case WM_CREATE:
 		hdc = GetDC(hwnd);
+		LoadFromFile(circleCollection);
+		break;
+	case WM_PAINT:
+		for_each(circleCollection.begin(), circleCollection.end(), [](Circle circle) { DrawCircle(hdc, circle.center.x, circle.center.y, circle.R); });
 		break;
 	case WM_LBUTTONUP:
 		int xC; xC = LOWORD(lparam);
 		int yC;  yC = HIWORD(lparam);
 
-		 c = GetCircle(circleCollection, xC, yC);
+		c = GetCircle(circleCollection, xC, yC);
 		if (c.R == -1)
 		{
 			circleCollection.push_back(DrawCircle(hdc, xC, yC, 5));
@@ -65,6 +88,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 		y = HIWORD(lparam);
 		break;
 	case  WM_CLOSE:
+		SaveToFile(circleCollection);
 		DestroyWindow(hwnd);
 		ReleaseDC(hwnd, hdc);
 		break;
@@ -153,4 +177,19 @@ Circle GetCircle(vector<Circle>& circles, int x, int y)
 		circles.erase(iter);
 	}
 	return c;
+}
+
+void SaveToFile(vector<Circle>& circles)
+{
+	ofstream out("data.txt");
+	out.clear();
+	copy(circles.begin(), circles.end(), ostream_iterator<Circle>(out, "\n"));
+	out.close();
+}
+
+void LoadFromFile(vector<Circle>& circles)
+{
+	ifstream out("data.txt");
+	copy(istream_iterator<Circle>(out), istream_iterator<Circle>(),back_inserter(circles));
+	out.close();
 }
